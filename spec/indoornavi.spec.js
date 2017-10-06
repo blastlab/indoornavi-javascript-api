b23169420b7b4b97952de96fc44e9b2ec6abfb86
Karol Gebert <kgebert@blastlab.co>
2017-10-06 13:49:19 +0200

    Event listener added, readme added navi-152

--- spec/indoornavi.spec.js
+++ spec/indoornavi.spec.js
@@ -1,5 +1,5 @@
 describe('IndoorNavi main module tests', function () {
-    it('Should throw an error when you try to toggle tag visibility when iFrame is not ready ', function () {
+    it('Should throw an error when you try to toggle tag visibility when iFrame is not ready', function () {
         // given
         let indoorNavi = new IndoorNavi();
 
@@ -9,10 +9,10 @@ describe('IndoorNavi main module tests', function () {
         };
 
         // then
-        expect(toTest).toThrow(new Error('IndoorNavi is not ready'));
+        expect(toTest).toThrow(new Error('IndoorNavi is not ready. Call load() first and then when promise resolves IndoorNavi will be ready.'));
     });
 
-    it('Should send message to iFrame when iFrame is ready', function() {
+    it('Should send message to iFrame when iFrame is ready and toggle tag is called', function() {
         // given
         let indoorNavi = new IndoorNavi();
         indoorNavi.isReady = true;
@@ -27,5 +27,37 @@ describe('IndoorNavi main module tests', function () {
         expect(Communication.send).toHaveBeenCalled();
         expect(DOM.getById).toHaveBeenCalled();
         expect(DOM.getByTagName).toHaveBeenCalled();
+    });
+
+    it('Should throw an error when you try to add event listener when iFrame is not ready', function() {
+        // given
+        let indoorNavi = new IndoorNavi();
+
+        // when
+        const toTest = function () {
+            indoorNavi.addEventListener('area', function() {});
+        };
+
+        // then
+        expect(toTest).toThrow(new Error('IndoorNavi is not ready. Call load() first and then when promise resolves IndoorNavi will be ready.'));
+    });
+
+    it('Should send message to iFrame and start listening on events when iFrame is ready and add event listener is called', function() {
+        // given
+        let indoorNavi = new IndoorNavi();
+        indoorNavi.isReady = true;
+        spyOn(Communication, 'send').and.stub();
+        spyOn(DOM, 'getById').and.stub();
+        spyOn(DOM, 'getByTagName').and.stub();
+        spyOn(Communication, 'listen').and.stub();
+
+        // when
+        indoorNavi.addEventListener('area', function() {});
+
+        // then
+        expect(Communication.send).toHaveBeenCalled();
+        expect(DOM.getById).toHaveBeenCalled();
+        expect(DOM.getByTagName).toHaveBeenCalled();
+        expect(Communication.listen).toHaveBeenCalled();
     });
 });
