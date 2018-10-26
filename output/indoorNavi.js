4db2cc9ed421deee9b31806229b17245a47c4705
bartoshko <blenart@blastlab.co>
2018-10-26 11:50:18 +0200

    bug in getArea fixed navi-451

--- output/indoorNavi.js
+++ output/indoorNavi.js
@@ -1471,7 +1471,7 @@ class INData {
                 const payloads = JSON.parse(data);
                 const areas = payloads.map(payload => {
                     return {
-                        id: payload.name,
+                        id: payload.id,
                         name: payload.name,
                         points: payload.points
                     }
