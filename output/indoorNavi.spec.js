b8a7d48cfcd5de07435321d447950ed31c7afe89
bartoshko <blenart@blastlab.co>
2018-11-23 11:39:02 +0100

    enum with error messages

--- output/indoorNavi.spec.js
+++ output/indoorNavi.spec.js
@@ -328,6 +328,16 @@ class Coordinates {
 }
 
 /**
+ * Enum with description of error codes
+ */
+const Error = Object.freeze({
+    /**
+     * Enum holding descriptions for error codes
+     */
+    NV_001: Symbol('Navigation route has zero length or there is no paths available on given map.')
+});
+
+/**
  * Global object representing events,
  * @namespace
  * @property {object} MOUSE - mouse events, ENUM like, object
---
+++ src/model/error.js
@@ -0,0 +1,9 @@
+/**
+ * Enum with description of error codes
+ */
+const Error = Object.freeze({
+    /**
+     * Enum holding descriptions for error codes
+     */
+    NV_001: Symbol('Navigation route has zero length or there is no paths available on given map.')
+});
